bf50d3716a0ddab397aeead0d0142e7b4d28c215
Masayoshi.Yamashita <masayoshi.yamashita@scsk.jp>
2016-07-27 16:06:31 +0900

    Remove unnecessary parameter.
    Refer to https://github.com/primecloud-controller-org/primecloud-controller/issues/68

--- src/pcc/api/address/add_aws_address.py
+++ src/pcc/api/address/add_aws_address.py
@@ -5,14 +5,11 @@ def command():
 
 def init_argument(parser):
     parser.add_argument("--platform-no", required=True)
-    parser.add_argument("--farm-no", required=True)
 
 def execute(requester, args):
     platform_no = args.platform_no
-    farm_no = args.farm_no
 
     parameters = {}
     parameters["PlatformNo"] = platform_no
-    parameters["FarmNo"] = farm_no
 
     return requester.execute("/AddAwsAddress", parameters)
--- src/pcc/api/address/delete_aws_address.py
+++ src/pcc/api/address/delete_aws_address.py
@@ -5,14 +5,11 @@ def command():
 
 def init_argument(parser):
     parser.add_argument("--address-no", required=True)
-    parser.add_argument("--farm-no", required=True)
 
 def execute(requester, args):
     address_no = args.address_no
-    farm_no = args.farm_no
 
     parameters = {}
     parameters["AddressNo"] = address_no
-    parameters["FarmNo"] = farm_no
 
     return requester.execute("/DeleteAwsAddress", parameters)
